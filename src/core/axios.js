700b8f735e834b54ba6e88665a28db49c0f3f5bd
senice222 <sasdasd920@gmail.com>
2024-08-14 22:17:44 +0300

    another url

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // https://kvik.cc
-export const url = 'https://kvik.cc/api';
+export const url = 'https://consultantnlgpanel.ru/api';
 const $api = axios.create({
     baseURL: url,
     headers: {
